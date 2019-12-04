e0a91e74c0c5c022ebc67a7668bde08cfaff7364
Diego Linares <linaresbda@gmail.com>
2019-12-03 19:31:45 -0600

    Se agrega servidor web

---
+++ server.js
@@ -0,0 +1,70 @@
+
+
+const Hapi = require('@hapi/hapi');
+// const Git = require('./util-git');
+
+// let git = new Git();
+
+class ServerClass {
+  constructor(routes) {
+    this.routes = routes;
+    this.init();
+  }
+
+  init() {
+    // Referencia Server Options: https://hapi.dev/api/?v=18.4.0#server.options
+    const server = Hapi.server({
+      port: 81,
+      host: 'localhost'
+    });
+    setRoutes(server);
+    setEvents(server);
+    server.start();
+  };
+}
+
+const setRoutes = (server) => {
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: async function (request, h) {
+      try {
+        // await git.log();
+      } catch (error) {
+
+      }
+      return 'Hello World!';
+    }
+  });
+}
+
+const setEvents = (server) => {
+  server.events.on('route', (route) => {
+    console.log(`New route added: ${route.path}`);
+  });
+  server.events.on('start', async () => {
+    console.log(`Server running on port: ${server.info.port}`);
+
+    // Logica para clonar el repositorio de Git o actualizarlo.
+    try {
+      // await git.clone('https://github.com/nodegit/nodegit', 'nodegit');
+    } catch (error) {
+      console.error(error.message);
+    }
+  });
+  server.events.on('stop', () => {
+    console.log(`Server stopped`);
+  });
+  server.events.on('response', (request) => {
+    console.log(`Response sent for request: ${request.info.id}`);
+  });
+  server.events.on('log', (event, tags) => {
+    if (tags.error) {
+      console.log(`Server error: ${event.error ? event.error.message : 'unknown'}`);
+    }
+  });
+}
+
+let Server = new ServerClass();
+
+module.exports = Server;
